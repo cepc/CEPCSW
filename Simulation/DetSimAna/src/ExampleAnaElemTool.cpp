872029dc2614a2d973731b9257dd2583892fd30d
lintao <lintao51@gmail.com>
2019-08-12 15:52:02 +0800

    WIP: add an example tool.

---
+++ Simulation/DetSimAna/src/ExampleAnaElemTool.cpp
@@ -0,0 +1,54 @@
+#include "ExampleAnaElemTool.h"
+
+DECLARE_COMPONENT(ExampleAnaElemTool)
+
+void
+ExampleAnaElemTool::BeginOfRunAction(const G4Run*) {
+
+}
+
+void
+ExampleAnaElemTool::EndOfRunAction(const G4Run*) {
+
+}
+
+void
+ExampleAnaElemTool::BeginOfEventAction(const G4Event*) {
+
+}
+
+void
+ExampleAnaElemTool::EndOfEventAction(const G4Event*) {
+
+}
+
+void
+ExampleAnaElemTool::PreUserTrackingAction(const G4Track*) {
+
+}
+
+void
+ExampleAnaElemTool::PostUserTrackingAction(const G4Track*) {
+
+}
+
+void
+ExampleAnaElemTool::UserSteppingAction(const G4Step*) {
+
+}
+
+StatusCode
+ExampleAnaElemTool::initialize() {
+    StatusCode sc;
+
+    return sc;
+}
+
+StatusCode
+ExampleAnaElemTool::finalize() {
+    StatusCode sc;
+
+    return sc;
+}
+
+
---
+++ Simulation/DetSimAna/src/ExampleAnaElemTool.h
@@ -0,0 +1,36 @@
+#ifndef ExampleAnaElemTool_h
+#define ExampleAnaElemTool_h
+
+#include "GaudiKernel/AlgTool.h"
+#include "DetSimInterface/IAnaElemTool.h"
+
+class ExampleAnaElemTool: public extends<AlgTool, IAnaElemTool> {
+
+public:
+
+    using extends::extends;
+
+    /// IAnaElemTool interface
+    // Run
+    virtual void BeginOfRunAction(const G4Run*) override;
+    virtual void EndOfRunAction(const G4Run*) override;
+
+    // Event
+    virtual void BeginOfEventAction(const G4Event*) override;
+    virtual void EndOfEventAction(const G4Event*) override;
+
+    // Tracking
+    virtual void PreUserTrackingAction(const G4Track*) override;
+    virtual void PostUserTrackingAction(const G4Track*) override;
+
+    // Stepping
+    virtual void UserSteppingAction(const G4Step*) override;
+
+
+    /// Overriding initialize and finalize
+    StatusCode initialize() override;
+    StatusCode finalize() override;
+
+};
+
+#endif
