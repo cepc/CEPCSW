86112aede14f67216a5e073d7a39a7c38acca174
zoujh <zoujh@ihep.ac.cn>
2019-08-14 17:38:49 +0800

    add reading test for plcio

--- Event/tests/write.cpp
+++ Event/tests/write.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "start writing test" << std::endl;
+    //std::cout << "start writing test" << std::endl;
 
     auto store = podio::EventStore();
     auto writer = podio::ROOTWriter("test.root", &store);
@@ -23,12 +23,37 @@ int main(int argc, char* argv[])
     const unsigned int nEvt = 100;
 
     for ( unsigned int i = 0; i < nEvt; ++i ) {
-        if ( i%10 == 0 ) {
+        //if ( i%10 == 0 ) {
             std::cout << "processing event " << i << std::endl;
-        }
+        //}
 
         auto header = plcio::EventHeader(i, -99, 9999, "SimDet");
         ehc.push_back(header);
+
+        for ( unsigned int i = 0; i < 10; ++i ) {
+            mcc.create();
+        }
+
+        for ( unsigned int i = 0; i < 4; ++i ) {
+            auto p = mcc[i];
+            for ( unsigned int j = 0; j < 4; ++j ) {
+                unsigned int idx = (2+j) + (i/2)*4;
+                p.addDaughter( mcc[idx] );
+                mcc[idx].addParent( p );
+            }
+        }
+
+        for ( auto p : mcc ) {
+            std::cout << " particle " << p.getObjectID().index << " has daughters: ";
+            for ( auto it = p.daughters_begin(), end = p.daughters_end(); it != end; ++it ) {
+                std::cout << " " << it->getObjectID().index;
+            }
+            std::cout << "  and parents: ";
+            for ( auto it = p.parents_begin(), end = p.parents_end(); it != end ; ++it ){
+                std::cout << " " << it->getObjectID().index;
+            }
+            std::cout << std::endl;
+        }
 
         //////////
         writer.writeEvent();
