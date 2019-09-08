d46dc0b4d29e262d1c7eaace85288cb0b7ead104
lintao <lintao51@gmail.com>
2019-09-08 10:28:07 +0800

    WIP: add a converter tool.

---
+++ Simulation/DetSimInterface/DetSimInterface/IG4PrimaryCnvTool.h
@@ -0,0 +1,22 @@
+#ifndef IG4PrimaryCnvTool_h
+#define IG4PrimaryCnvTool_h
+
+// IG4PrimaryCnvTool:
+//     convert an event in other formats to an event in G4 format.
+// The G4Event object is managed by Geant4.
+
+#include "GaudiKernel/AlgTool.h"
+
+class G4Event;
+
+class IG4PrimaryCnvTool: virtual public IAlgTool {
+public:
+
+    DeclareInterfaceID(IG4PrimaryCnvTool, 0, 1);
+
+    virtual ~IG4PrimaryCnvTool() {};
+
+    virtual mutate(G4Event* anEvent) = 0;
+};
+
+#endif
