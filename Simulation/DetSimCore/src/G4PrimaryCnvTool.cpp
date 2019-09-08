fe188f7f90e5c6d1cd5950942095091b8a03bd44
lintao <lintao51@gmail.com>
2019-09-08 11:26:10 +0800

    WIP: try to load event in a tool.

--- Simulation/DetSimCore/src/G4PrimaryCnvTool.cpp
+++ Simulation/DetSimCore/src/G4PrimaryCnvTool.cpp
@@ -6,6 +6,15 @@ DECLARE_COMPONENT(G4PrimaryCnvTool)
 
 bool G4PrimaryCnvTool::mutate(G4Event* anEvent) {
 
+    auto mcCol = m_mcParCol.get();
+    info() << "Start a new event: " << endmsg;
+    for ( auto p : *mcCol ) {
+        info() << p.getObjectID().index << " : [";
+        for ( auto it = p.daughters_begin(), end = p.daughters_end(); it != end; ++it ) {
+            info() << " " << it->getObjectID().index;
+        }
+        info() << " ]; " << endmsg;
+    }
 
     return true;
 }
--- Simulation/DetSimCore/src/G4PrimaryCnvTool.h
+++ Simulation/DetSimCore/src/G4PrimaryCnvTool.h
@@ -3,6 +3,10 @@
 
 #include "GaudiKernel/AlgTool.h"
 #include "DetSimInterface/IG4PrimaryCnvTool.h"
+#include "FWCore/DataHandle.h"
+
+#include "plcio/EventHeaderCollection.h"
+#include "plcio/MCParticleCollection.h"
 
 class G4PrimaryCnvTool: public extends<AlgTool, IG4PrimaryCnvTool> {
 public:
@@ -10,6 +14,9 @@ public:
     using extends::extends;
 
     bool mutate(G4Event* anEvent) override;
+
+private:
+    DataHandle<plcio::MCParticleCollection> m_mcParCol{"MCParticleCol", Gaudi::DataHandle::Reader, this};
 
 };
 
