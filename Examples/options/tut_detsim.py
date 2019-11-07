b59a43096a3bb178181737e0a1ff512f27e59fbf
lintao <lintao51@gmail.com>
2019-11-07 18:56:34 +0800

    update the job option for detsim.

--- Examples/options/tut_detsim.py
+++ Examples/options/tut_detsim.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+import os
+print(os.environ["DD4HEP_LIBRARY_PATH"])
+import sys
+# sys.exit(0)
+
 from Gaudi.Configuration import *
 
 ##############################################################################
@@ -14,10 +19,9 @@ dsvc = CEPCDataSvc("EventDataSvc")
 from Configurables import GenAlgo
 
 genalg = GenAlgo("read")
-genalg.Input = "/junofs/users/wxfang/CEPC/whizard_apply/ee/ee.slcio"
-genalg.FileFormat = "slcio"
-genalg.PrintEvent = True # true for printing mc info
-genalg.WriteFile = True  # true for writting info to root
+genalg.Input = "/cefs/data/stdhep/CEPC250/2fermions/E250.Pbhabha.e0.p0.whizard195/bhabha.e0.p0.00001.stdhep"
+genalg.FileFormat = "stdhep"
+genalg.PrintEvent = False # true for printing mc info
 
 ##############################################################################
 # Detector Simulation
@@ -36,7 +40,7 @@ detsimalg = DetSimAlg("DetSimAlg")
 # detsimalg.VisMacs = ["vis.mac"]
 
 detsimalg.RunCmds = [
-    "/tracking/verbose 1",
+#    "/tracking/verbose 1",
 ]
 detsimalg.AnaElems = [
     # example_anatool.name()
@@ -46,14 +50,30 @@ detsimalg.RootDetElem = "WorldDetElemTool"
 
 from Configurables import AnExampleDetElemTool
 example_dettool = AnExampleDetElemTool("AnExampleDetElemTool")
-example_dettool.detxml = "/cvmfs/sft.cern.ch/lcg/releases/DD4hep/01-08-c926f/x86_64-slc6-gcc62-opt/DDDetectors/compact/SiD.xml"
+
+# geometry_option = "CepC_v4-onlyTracker.xml"
+geometry_option = "CepC_v4-onlyVXD.xml"
+
+if os.getenv("DETCEPCV4ROOT"):
+    example_dettool.detxml = os.path.join(os.getenv("DETCEPCV4ROOT"), "compact", geometry_option)
+else:
+    print("Can't find the geometry. Please setup envvar DETCEPCV4ROOT." )
+    sys.exit(-1)
+
+##############################################################################
+# POD I/O
+##############################################################################
+from Configurables import PodioOutput
+out = PodioOutput("outputalg")
+out.filename = "test-detsim10.root"
+out.outputCommands = ["keep *"]
 
 ##############################################################################
 # ApplicationMgr
 ##############################################################################
 
 from Configurables import ApplicationMgr
-ApplicationMgr( TopAlg = [genalg, detsimalg],
+ApplicationMgr( TopAlg = [genalg, detsimalg, out],
                 EvtSel = 'NONE',
                 EvtMax = 10,
                 ExtSvc = [dsvc],
