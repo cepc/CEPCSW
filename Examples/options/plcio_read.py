e15af5d660c25b41a1d10ba5497325aeac72d8eb
lintao <lintao51@gmail.com>
2020-05-11 15:54:54 +0800

    WIP: migrate to K4DataSvc.

--- Examples/options/plcio_read.py
+++ Examples/options/plcio_read.py
@@ -2,8 +2,8 @@
 
 from Gaudi.Configuration import *
 
-from Configurables import CEPCDataSvc
-dsvc = CEPCDataSvc("EventDataSvc", input="test.root")
+from Configurables import K4DataSvc
+dsvc = K4DataSvc("EventDataSvc", input="test.root")
 
 from Configurables import PlcioReadAlg
 alg = PlcioReadAlg("PlcioReadAlg")
