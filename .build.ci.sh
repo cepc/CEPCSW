04be2ac2796022a2d2b367fdfa31cee1618931c0
lintao <lintao51@gmail.com>
2020-10-16 00:08:26 +0800

    WIP: support both lcg and key4hep stack.

--- .build.ci.sh
+++ .build.ci.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # This is wrapper to run the build.sh on CI
 
-source setup.sh
-./build.sh >& mylog.sh &
-buildpid=$!
+logfile=mylog.txt
+
+if [ "$LCG_RELEASE" = "KEY4HEP_STACK" ]; then
+    logfile=mylog-k4.sh
+    source /cvmfs/sw.hsf.org/key4hep/setup.sh
+    ./build-k4.sh >& ${logfile} &
+    buildpid=$!
+else
+    source setup.sh
+    ./build.sh >& ${logfile} &
+    buildpid=$!
+fi
 
 while ps -p $buildpid 2>/dev/null ; do
     sleep 60
 done
 
-tail -n100 mylog.sh
+tail -n100 ${logfile}
 
