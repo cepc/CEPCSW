4f52b3943d7335b530e01eaaa9cdafa6ce22d6fc
lintao <lintao51@gmail.com>
2020-12-07 17:53:47 +0800

    WIP: setup DummyFastSimG4Tool

--- Simulation/DetSimFastModel/src/DummyFastSimG4Tool.cpp
+++ Simulation/DetSimFastModel/src/DummyFastSimG4Tool.cpp
@@ -1,1 +1,20 @@
 #include "DummyFastSimG4Tool.h"
+
+#include "G4VFastSimulationModel.hh"
+
+StatusCode DummyFastSimG4Tool::initialize() {
+    StatusCode sc;
+
+    return sc;
+}
+
+StatusCode DummyFastSimG4Tool::finalize() {
+    StatusCode sc;
+
+    return sc;
+}
+
+bool DummyFastSimG4Tool::CreateFastSimulationModel() {
+
+    return true;
+}
--- Simulation/DetSimFastModel/src/DummyFastSimG4Tool.h
+++ Simulation/DetSimFastModel/src/DummyFastSimG4Tool.h
@@ -1,6 +1,20 @@
 #ifndef DummyFastSimG4Tool_h
 #define DummyFastSimG4Tool_h
 
+#include "GaudiKernel/AlgTool.h"
+#include "DetSimInterface/IFastSimG4Tool.h"
 
+class DummyFastSimG4Tool: public extends<AlgTool, IFastSimG4Tool> {
+public:
+    using extends::extends;
+
+    StatusCode initialize() override;
+    StatusCode finalize() override;
+
+    bool CreateFastSimulationModel() override;
+
+private:
+
+};
 
 #endif
