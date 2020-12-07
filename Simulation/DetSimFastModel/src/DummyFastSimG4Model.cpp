666145faea3308bb04ababddd422ef6b4a2e4774
lintao <lintao51@gmail.com>
2020-12-07 18:05:24 +0800

    WIP: setup DummyFastSimG4Model

--- Simulation/DetSimFastModel/src/DummyFastSimG4Model.cpp
+++ Simulation/DetSimFastModel/src/DummyFastSimG4Model.cpp
@@ -1,1 +1,22 @@
 #include "DummyFastSimG4Model.h"
+
+DummyFastSimG4Model::DummyFastSimG4Model(G4String aModelName, G4Region* aEnvelope)
+    : G4VFastSimulationModel(aModelName, aEnvelope) {
+
+}
+
+DummyFastSimG4Model::~DummyFastSimG4Model() {
+
+}
+
+G4bool DummyFastSimG4Model::IsApplicable(const G4ParticleDefinition& aParticle) {
+    return true;
+}
+
+G4bool DummyFastSimG4Model::ModelTrigger(const G4FastTrack& aFastTrack) {
+    return true;
+}
+
+void DummyFastSimG4Model::DoIt(const G4FastTrack& aFastTrack, G4FastStep& aFastStep) {
+
+}
