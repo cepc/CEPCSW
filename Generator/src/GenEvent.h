d0b5b6d5fec132eddaf64267b8a325965ed86f79
lintao <lintao51@gmail.com>
2021-02-25 17:25:56 +0800

    Fix #153: avoid to use the assign operator of MCParticleCollection.

--- Generator/src/GenEvent.h
+++ Generator/src/GenEvent.h
@@ -10,17 +10,18 @@ class GenEvent{
         //GenEvent();
         GenEvent(edm4hep::MCParticleCollection& mcCol);
         ~GenEvent();
-        void SetEventHeader(long event_id_, long run_id_, float time_, std::string det_name_);
+        void SetEventHeader(long event_id_, long run_id_, float time_, const std::string& det_name_);
         //void SetMCCollection(edm4hep::MCParticleCollection vec_);
         long getID();
         long getRun();
         long getTime();
         void ReSet();
         std::string getName();
-        edm4hep::MCParticleCollection getMCVec();
+        edm4hep::MCParticleCollection& getMCVec();
         edm4hep::MCParticleCollection& m_mc_vec;
         //edm4hep::MCParticleCollection m_mc_vec;
     private:
+
         long m_event_id;
         long m_run_id;
         float m_time;
